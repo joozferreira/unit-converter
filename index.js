1de3da916dea2df03914f74273864d4924cc50fd
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2024-02-24 22:39:23 +0000

    Cretion of dark mode

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
+/* Conversion units
+1 meter = 3.2808399 feet
+1 liter = 0.264172052 gallon
+1 kilogram = 2.20462262 pound
 */
 
+const body = document.getElementsByTagName("body")[0];
+const sectionList = Array.from(document.getElementsByTagName("section"));
 const lengthConversion = document.getElementById("length-conversion");
 const volumeConversion = document.getElementById("volume-conversion");
 const massConversion = document.getElementById("mass-conversion");
 const quantity = document.getElementById("quantity");
 const convertBtn = document.getElementById("convert");
+const lightDarkMode = document.getElementById("light-dark-mode");
+const mode = document.getElementById("mode");
 
 // Ensures that only numbers are input on the input field
 quantity.addEventListener("keyup", (event) => {
@@ -31,3 +35,19 @@ function converter() {
   volumeConversion.textContent = `${input} liters = ${convertedGallon} gallons | ${input} gallons = ${convertedLiter} liters`;
   massConversion.textContent = `${input} kilos = ${convertedPound} pounds | ${input} pounds = ${convertedKilo} kilos`;
 }
+
+mode.addEventListener("click", () => {
+  if (lightDarkMode.classList.contains("dark")) {
+    lightDarkMode.setAttribute("src", "./svg-icons/moon-icon.svg");
+    lightDarkMode.classList.toggle("dark");
+    lightDarkMode.classList.toggle("svg-color");
+    body.classList.toggle("dark-mode");
+    sectionList.forEach((item) => item.classList.toggle("dark-section"));
+  } else {
+    lightDarkMode.setAttribute("src", "./svg-icons/day-sunny-icon.svg");
+    lightDarkMode.classList.toggle("dark");
+    lightDarkMode.classList.toggle("svg-color");
+    body.classList.toggle("dark-mode");
+    sectionList.forEach((item) => item.classList.toggle("dark-section"));
+  }
+});
